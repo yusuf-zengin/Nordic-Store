ae24f2e31e7e007953c3ff0cda8e2c3930444898
Yusuf İslam Zengin <yusufislamzengin@Yusuf-MacBook-Air.local>
2023-02-18 16:56:27 +0300

    added Tailwind

--- packages/frontend/src/app/app.tsx
+++ packages/frontend/src/app/app.tsx
@@ -6,8 +6,8 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
-      <NxWelcome title="frontend" />
-
+      <div className={'bg-black w-10 h-10'}></div>
+    
       <div />
     </>
   );
