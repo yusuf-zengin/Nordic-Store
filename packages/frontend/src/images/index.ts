573bf00883149edf96ea809418eea2ea90aa59ce
Yusuf İslam Zengin <yusufislamzengin@Yusuf-MacBook-Air.local>
2023-02-19 15:46:43 +0300

    sliders are almost fully autimated but not done yet

--- packages/frontend/src/images/index.ts
+++ packages/frontend/src/images/index.ts
@@ -4,7 +4,7 @@ export const viewImages = async () => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     params: {
       query: 'view',
-      per_page: 10,
+      per_page: 20,
       client_id: '27CzhmfJukpuBav0LqSFuZsgVxc9X5XtORKfv1JrkAQ',
     },
   });
